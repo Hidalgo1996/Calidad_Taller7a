3f5fb2471ea56c035b5bd7b45052ac215c2b0a78
Hidalgo1996 <GeForce210_1996@hotmail.com>
2023-06-19 07:15:54 -0500

    Filtro y ejecución del registro de ventas

--- src/app/components/registrar-ventas/registrar-ventas.component.ts
+++ src/app/components/registrar-ventas/registrar-ventas.component.ts
@@ -21,15 +21,53 @@ export class RegistrarVentasComponent implements OnInit{
   cantidad_stock="";
   categorias: Categoria[] = [];
   productos: Producto[] = [];
-  
+  productosFiltrados: Producto[] = [];
+    
   //Servicio de llamado
   constructor(private recibeCategoriaServices: Contienecategoria, private reciveProductoServices: ContieneProductos) {}
 
   ngOnInit(): void {
-    //Recibe categorias
     this.categorias = this.recibeCategoriaServices.listCategoria;
-    //Recibe registro productos
     this.productos = this.reciveProductoServices.listProducto;
+
+    this.form = new FormGroup({
+      categoria: new FormControl(''),
+      producto: new FormControl(''),
+      cantidad: new FormControl(''),
+      cantidadCompra: new FormControl({ value: '', disabled: true })
+    });
+  }
+
+  filtrarProductosPorCategoria(event: Event) {
+    const categoriaSeleccionada = (event.target as HTMLSelectElement).value;
+    this.productosFiltrados = this.productos.filter(producto => producto.categoria === categoriaSeleccionada);
+    this.form.get('producto')?.setValue(''); // Reiniciamos el valor del producto seleccionado
+  }
+
+  actualizarPrecioVenta(nombreProducto: string): void {
+    const productoSeleccionado = this.productos.find(producto => producto.nombre_producto === nombreProducto);
+
+    if (productoSeleccionado) {
+      this.nombre_producto = nombreProducto;
+      this.precio_venta = productoSeleccionado.precio_venta.toString();
+    } else {
+      this.nombre_producto = "";
+      this.precio_venta = "";
+    }
+
+    this.form.controls['cantidadCompra'].setValue('');
+  }
+
+  calcularPrecioTotal() {
+    const cantidad = this.form.get('cantidad')?.value;
+    const productoSeleccionado = this.productos.find(producto => producto.nombre_producto === this.form.get('producto')?.value);
+  
+    if (cantidad && productoSeleccionado) {
+      const precioTotal = cantidad * productoSeleccionado.precio_venta;
+      this.form.get('cantidadCompra')?.setValue(precioTotal.toFixed(2));
+    } else {
+      this.form.get('cantidadCompra')?.setValue('');
+    }
   }
 
 }
