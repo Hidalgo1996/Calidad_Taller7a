afa95f4d490f37869746b3e9555ec63bf87ca12b
Hidalgo1996 <GeForce210_1996@hotmail.com>
2023-06-19 03:14:13 -0500

    Avance de registro de productos

--- src/app/components/registrar-productos/registrar-productos.component.ts
+++ src/app/components/registrar-productos/registrar-productos.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Categoria } from 'src/app/Models/Categoria';
+import { Contienecategoria } from 'src/app/Servicios/Contiene-categoria';
 
 @Component({
   selector: 'app-registrar-productos',
   templateUrl: './registrar-productos.component.html',
   styleUrls: ['./registrar-productos.component.css']
 })
-export class RegistrarProductosComponent {
+export class RegistrarProductosComponent implements OnInit{
+  categorias: Categoria[] = [];
+  
+  //Servicio de llamado
+  constructor(private recibeCategoriaServices: Contienecategoria) {}
+
+  ngOnInit(): void {
+    this.categorias = this.recibeCategoriaServices.listCategoria;
+  }
 
 }
