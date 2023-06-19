e197b299d5f063cda9f0145b591d532370d0b59d
Hidalgo1996 <GeForce210_1996@hotmail.com>
2023-06-19 05:42:20 -0500

    Implementación de productos agotados o prox agotar

--- src/app/components/listar-inventarios-agotados/listar-inventarios-agotados.component.ts
+++ src/app/components/listar-inventarios-agotados/listar-inventarios-agotados.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/app/Models/Producto';
+import { ContieneProductos } from 'src/app/Servicios/contiene-productos';
 
 @Component({
   selector: 'app-listar-inventarios-agotados',
   templateUrl: './listar-inventarios-agotados.component.html',
   styleUrls: ['./listar-inventarios-agotados.component.css']
 })
-export class ListarInventariosAgotadosComponent {
+export class ListarInventariosAgotadosComponent implements OnInit{
+  productos: Producto[] = [];
 
+  //INYECTA SERVICIO
+  constructor(private contieneProductos: ContieneProductos) { }
+
+  //TRAE LA VISTA
+  ngOnInit(): void {
+    this.productos = this.contieneProductos.listProducto;
+  }
+
+  siStockInsufficient(stock: number): boolean {
+    return stock < 10;
+  }
 }
