b6b4dcc0ecf41781b1fdc0384fb66472c46006af
Hidalgo1996 <GeForce210_1996@hotmail.com>
2023-06-19 05:10:49 -0500

    Finalización del registrar y listar productos

--- src/app/components/listar-inventario-producto/listar-inventario-producto.component.ts
+++ src/app/components/listar-inventario-producto/listar-inventario-producto.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/app/Models/Producto';
+import { ContieneProductos } from 'src/app/Servicios/contiene-productos';
+
 
 @Component({
   selector: 'app-listar-inventario-producto',
   templateUrl: './listar-inventario-producto.component.html',
   styleUrls: ['./listar-inventario-producto.component.css']
 })
-export class ListarInventarioProductoComponent {
+export class ListarInventarioProductoComponent implements OnInit{
+  productos: Producto[] = [];
 
+  //INYECTA SERVICIO
+  constructor(private contieneProductos: ContieneProductos) { }
+
+  //TRAE LA VISTA
+  ngOnInit(): void {
+    this.productos = this.contieneProductos.listProducto;
+  }
 }
